b53e2efe0edd36dda1f92448b9196d1ac319a2bd
Gyokan <gyokan.nezhdetov@gmail.com>
2018-07-04 18:25:35 +0300

    Bootstrapping the app

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `<h3>Hello Angular</h3>`
+})
+export class EventsAppComponent {
+  title = 'app';
+}
