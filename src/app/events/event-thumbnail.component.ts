73687b7e73fa784afd3a5f34dadcbd03f811a6ff
Gyokan <gyokan.nezhdetov@gmail.com>
2018-07-07 11:03:42 +0300

    Added multiple events

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -15,6 +15,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     </div>
   `,
   styles: [`
+    .thumbnail { min-height: 210px; }
     .pad-left: { margin-left: 10px; }
     .well div { color: #BBB; }
   `]
