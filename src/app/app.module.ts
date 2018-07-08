de22cc7ba1f87a574221a2703059b05ecb004969
Gyokan <gyokan.nezhdetov@gmail.com>
2018-07-08 18:27:41 +0300

    Added event service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { EventsAppComponent } from './events-app.component';
 import { EventListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   imports: [
@@ -16,6 +17,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -4,17 +4,27 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'event-thumbnail',
   template: `
     <div class="well horewell thumbnail">
-      <h2>{{event.name}}</h2>
-      <div>Date: {{event.date}}</div>
-      <div>Time: {{event.time}}</div>
-      <div>Price: \${{event.price}}</div>
-      <div>
-        <span>Location: {{event.location.address}}</span> 
-        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{event?.price}}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span> 
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
       </div>
     </div>
   `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left: { margin-left: 10px; }
     .well div { color: #BBB; }
@@ -25,5 +35,10 @@ export class EventThumbnailComponent {
 
   logFoo() {
     console.log('foo');
+  }
+  
+  getStartTimeClass() {
+    const isEarlyStart = this.event && this.event.time === "8:00 am";
+    return { green: isEarlyStart, bold: isEarlyStart }
   }
 }
