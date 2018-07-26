839176804b0390aaf8c50ad591e1f9235ce6e76b
Gyokan Syuleymanov <gyokan.nezhdetov@gmail.com>
2018-07-26 10:58:29 +0300

    Added profile reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,35 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+
+  constructor(private router: Router, private authService: AuthService) {
+
+  }
+
+  ngOnInit(): void {
+    const firstName = new FormControl(this.authService.currentUser.userName);
+    const lastName = new FormControl(this.authService.currentUser.lastName);
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+    this.router.navigate(['events']);
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
 
 }
