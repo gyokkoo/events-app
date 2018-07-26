d8d66e0585d8bd235200442d95832aca1ca95923
Gyokan Syuleymanov <gyokan.nezhdetov@gmail.com>
2018-07-26 10:27:59 +0300

    Added login form validations

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,11 +3,14 @@ import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styles: ['em { float: right; color: #E05C65; padding-left: 10px; }']
 })
 export class LoginComponent {
-  username;
+  userName;
   password;
+  mouseoverLogin;
+
   constructor(private authService: AuthService, private router: Router) {
 
   }
