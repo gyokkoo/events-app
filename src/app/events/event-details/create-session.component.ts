d28693a200f783b4f330a2be0953477b1cddd562
Gyokan Syuleymanov <gyokan.nezhdetov@gmail.com>
2018-07-29 23:35:25 +0300

    Added custom word validator

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ISession} from '../shared';
+import {ISession, restrictedWords} from '../shared/index';
 
 @Component({
   templateUrl: './create-session.component.html',
@@ -49,7 +49,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
     this.newSessionForm = new FormGroup({
       name: this.name,
@@ -59,6 +59,7 @@ export class CreateSessionComponent implements OnInit {
       abstract: this.abstract,
     });
   }
+
 
   saveSession(formValues): void {
     const session: ISession = {
