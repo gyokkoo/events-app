685bced262aed0eeb637333261d497b956573e52
Nezhdetov <gyokan.nezhdetov@gmail.com>
2018-08-05 13:30:52 +0300

    Implemented filtering functionality

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,27 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {ISession} from '../shared';
 
 @Component({
   selector: 'app-session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
+
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(
+        session => session.level.toLocaleLowerCase() === filter);
+    }
+  }
 }
