9ece91c5d63a2b89a01dac8c8d559ed19ce62c4d
Nezhdetov <gyokan.nezhdetov@gmail.com>
2018-09-02 14:18:15 +0300

    Search issue fix

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component, ElementRef, Inject, Input, ViewChild} from '@angular/core';
+import { JQ_TOKEN } from './jQuery.service';
 
 @Component({
   selector: 'app-simple-modal',
   template: `
-    <div id="{{elementId}}" class="modal fade" tabindex="-1">
+    <div id="{{elementId}}" #modalContainer class="modal fade" tabindex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
@@ -12,7 +13,7 @@ import {Component, Input} from '@angular/core';
             </button>
             <h4 class="modal-title">{{title}}</h4>
           </div>
-          <div class="modal.body">
+          <div class="modal-body" (click)="closeModal()">
             <ng-content></ng-content>
           </div>
         </div>
@@ -29,4 +30,11 @@ import {Component, Input} from '@angular/core';
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalContainer') containerEl: ElementRef;
+
+  constructor(@Inject(JQ_TOKEN) private $: any) {}
+
+  closeModal() {
+    this.$(this.containerEl.nativeElement).modal('hide');
+  }
 }
--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {EventService} from '../shared/event.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 import {IEvent, ISession} from '../shared/event.model';
 
 @Component({
@@ -31,10 +31,6 @@ export class EventDetailsComponent implements OnInit {
   constructor(private eventService: EventService, private route: ActivatedRoute) {
   }
 
-  ngOnInit () {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
-  }
-
   addSession() {
     this.addMode = true;
   }
@@ -49,5 +45,12 @@ export class EventDetailsComponent implements OnInit {
 
   cancelAddSession() {
     this.addMode = false;
+  }
+
+  ngOnInit () {
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    });
   }
 }
