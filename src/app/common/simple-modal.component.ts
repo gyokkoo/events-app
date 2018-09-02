2a4d2873b4028fdb2d7723c0f8d8746008c11e1d
Nezhdetov <gyokan.nezhdetov@gmail.com>
2018-09-02 14:22:15 +0300

    Close search when clicked

--- src/app/common/simple-modal.component.ts
+++ src/app/common/simple-modal.component.ts
@@ -30,11 +30,14 @@ import { JQ_TOKEN } from './jQuery.service';
 export class SimpleModalComponent {
   @Input() title: string;
   @Input() elementId: string;
+  @Input() closeOnBodyClick: string;
   @ViewChild('modalContainer') containerEl: ElementRef;
 
   constructor(@Inject(JQ_TOKEN) private $: any) {}
 
   closeModal() {
-    this.$(this.containerEl.nativeElement).modal('hide');
+    if (this.closeOnBodyClick.toLocaleLowerCase() === "true") {
+      this.$(this.containerEl.nativeElement).modal('hide');
+    }
   }
 }
